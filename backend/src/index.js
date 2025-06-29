1e65637b3467e65259385d00837f792b166a0bac
Muhammad Usman <muhammadusmanramzan586@gmail.com>
2025-06-29 21:31:33 +0500

    add configuration for the deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -23,9 +24,15 @@ app.use(
 );
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
