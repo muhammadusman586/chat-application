ebe06ec09aa28a6736973fd950eb7560962189f9
Muhammad Usman <muhammadusmanramzan586@gmail.com>
2025-06-29 21:47:28 +0500

    bug fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,8 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 import { connectDB } from "./lib/db.js";
-import { app,server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 dotenv.config();
-
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -29,9 +28,12 @@ const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
