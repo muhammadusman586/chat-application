db71eefd856bdafbe860f8a4924307705f230488
Muhammad Usman <muhammadusmanramzan586@gmail.com>
2025-07-01 20:45:31 +0500

    issues fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
-import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -21,9 +24,6 @@ app.use(
     credentials: true,
   })
 );
-
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -37,6 +37,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(PORT, () => {
-  console.log(`Server is running on Port ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
