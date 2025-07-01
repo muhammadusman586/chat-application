6fbfd1be9c1de9bcad3b116f7bf5c8a8e95569cd
Muhammad Usman <muhammadusmanramzan586@gmail.com>
2025-07-01 20:49:43 +0500

    route fixed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,5 @@
 import express from "express";
-import {
-  checkAuth,
-  login,
-  logout,
-  signup,
-  updateProfile,
-} from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -13,6 +7,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
 router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, checkAuth);
